7a9ed17ebcf02f2ac9af46b0f87167b11e035b4b
hissoune <khalidhissoune962@gmail.com>
2025-05-09 15:29:16 +0100

    refactor(auth): update auth module configuration

--- apps/auth-service/src/auth-service.module.ts
+++ apps/auth-service/src/auth-service.module.ts
@@ -2,8 +2,8 @@ import { Module } from '@nestjs/common';
 import { AuthServiceController } from './auth-service.controller';
 import { AuthServiceService } from './auth-service.service';
 import { ConfigModule } from '@nestjs/config';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModule } from './user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './user/entities/user.entity';
 
 @Module({
@@ -15,18 +15,18 @@ import { User } from './user/entities/user.entity';
   
      TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 6432,
-      username: 'postgres',
-      password: '123',
-      database: 'test_db',
+      host: process.env.DB_HOST || 'localhost',
+      port: Number(process.env.DB_PORT) || 6432,
+      username: process.env.DB_USER || 'user',
+      password: process.env.DB_PASSWORD || 'password',
+      database: process.env.DB_NAME || 'dbname',
       entities: [User],
       synchronize: true,
       logging: true,
-     
+      logger: 'advanced-console',
     }),
+    
     UserModule,
- 
 
   ],
   controllers: [AuthServiceController],
