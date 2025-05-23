4e1a3c653df964ab2384e64ae72d3e2976f0d6e0
hissoune <khalidhissoune962@gmail.com>
2025-05-23 10:29:45 +0100

    refactor(auth/user): improve user module structure

--- apps/auth-service/src/user/user.module.ts
+++ apps/auth-service/src/user/user.module.ts
@@ -3,10 +3,12 @@ import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
+import { LogtailModule } from '../logtail/logtail.module';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([User]),
+    LogtailModule
   ],
   controllers: [UserController],
   providers: [UserService],
