e755cbd3bf3160e9a674fcd64ca1c756097ac705
hissoune <khalidhissoune962@gmail.com>
2025-05-23 10:29:45 +0100

    feat(auth): update auth controller endpoints

--- apps/auth-service/src/auth-service.controller.ts
+++ apps/auth-service/src/auth-service.controller.ts
@@ -1,12 +1,21 @@
 import { Controller, Get } from '@nestjs/common';
 import { AuthServiceService } from './auth-service.service';
 
+
+
+
+
 @Controller()
 export class AuthServiceController {
   constructor(private readonly authServiceService: AuthServiceService) {}
 
   @Get('health')
   healthcheck(): string {
+    return this.authServiceService.getHello();
+  }
+
+  @Get('test')
+  test(): string {
     return this.authServiceService.getHello();
   }
 }
