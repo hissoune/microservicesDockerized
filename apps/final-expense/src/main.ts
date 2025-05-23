dd4f98b5f8845306a3f4c1e8902fb55a08280ac7
hissoune <khalidhissoune962@gmail.com>
2025-05-23 11:54:11 +0100

    refactor(expense): improve expense service bootstrap

--- apps/final-expense/src/main.ts
+++ apps/final-expense/src/main.ts
@@ -1,10 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { LogginInterceptor } from './logginInterseptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  
+  app.useGlobalInterceptors(new LogginInterceptor());
 
   await app.listen(process.env.PORT ?? 3000);
 }
