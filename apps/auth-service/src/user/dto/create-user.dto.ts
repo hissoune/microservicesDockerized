4afdfeb6df635d46ccc61112ba42c1324a6c13f8
hissoune <khalidhissoune962@gmail.com>
2025-05-23 10:29:45 +0100

    feat(auth/user): enhance user creation DTO validation

--- apps/auth-service/src/user/dto/create-user.dto.ts
+++ apps/auth-service/src/user/dto/create-user.dto.ts
@@ -1,1 +1,20 @@
-export class CreateUserDto {}
+import { IsEmail, IsNotEmpty, IsString } from "class-validator";
+
+
+
+export class CreateUserDto {
+
+    @IsString()
+    @IsNotEmpty()
+    name: string;
+
+    @IsString()
+    @IsNotEmpty()
+    @IsEmail()
+    email: string;
+   
+    @IsString()
+    @IsNotEmpty()
+    
+    password: string;
+}
