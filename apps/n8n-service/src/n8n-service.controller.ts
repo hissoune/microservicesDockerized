504dbb5d19e5fab6c2edf7539af9866b73b3e9e4
hissoune <khalidhissoune962@gmail.com>
2025-05-27 09:45:03 +0100

    ADD: adding the functionalities of the crud of the workflow

--- apps/n8n-service/src/n8n-service.controller.ts
+++ apps/n8n-service/src/n8n-service.controller.ts
@@ -1,6 +1,5 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
 import { N8nServiceService } from './n8n-service.service';
-import axios from 'axios';
 
 @Controller()
 export class N8nServiceController {
@@ -10,12 +9,42 @@ export class N8nServiceController {
   getHello(): string {
     return "N8n Service is running!";
   }
+ 
+  @Get('get-workflows')
+  async getWorkflows() {
+    return this.n8nServiceService.getWorkflows();
+  }
 
 
+
+  @Get('get-workflow/:id')
+  async getWorkflow(@Param() param: any) {
+      console.log("Fetching workflow with ID:", param.id);
+    
+    return this.n8nServiceService.getWorkflow(param.id);
+  }
+  
+  
   @Post('create-workflow')
   async createWorkflow(@Body() body: any) {
     console.log("Creating workflow with data:", body);
     
    return this.n8nServiceService.createWorkflow(body);
   }
+
+ @Patch('update-workflow')
+  async updateWorkflow(@Body() body: any) {
+    console.log("Updating workflow with data:", body);
+    
+    return this.n8nServiceService.updateWorkflow(body);
+  }
+
+  @Delete('delete-workflow/:id')
+  async deleteWorkflow(@Param() workflowId: any) {
+ console.log("Deleting workflow with ID:", workflowId.id);
+ 
+     return this.n8nServiceService.deleteWorkflow(workflowId.id);
+
+  }
+
 }
--- apps/n8n-service/src/n8n-service.service.ts
+++ apps/n8n-service/src/n8n-service.service.ts
@@ -6,6 +6,33 @@ export class N8nServiceService {
   getHello(): string {
     return 'Hello World!';
   }
+ 
+   
+  async getWorkflows() {
+    const response = await axios.get(
+      `${process.env.N8N_SERVICE_URL}/api/v1/workflows`,
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          'X-N8N-API-KEY': `${process.env.N8N_API_KEY}`, 
+        },
+      }
+    );
+    return response.data;
+  }
+
+  async getWorkflow(workflowId: string) {
+    const response = await axios.get(
+      `${process.env.N8N_SERVICE_URL}/api/v1/workflows/${workflowId}`,
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          'X-N8N-API-KEY': `${process.env.N8N_API_KEY}`, // Ensure you have set N8N_API_KEY in your environment variables
+        },
+      }
+    );
+    return response.data;
+  }
 
   async createWorkflow(workflow:any){
    
@@ -23,4 +50,35 @@ export class N8nServiceService {
    
   return response.data;
   }
+
+  async updateWorkflow(workflow:any){
+    const { id, ...workflowwithoutId } = workflow;
+    const response = await axios.put(
+      `${process.env.N8N_SERVICE_URL}/api/v1/workflows/${workflow.id}`, workflowwithoutId,
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          'X-N8N-API-KEY': `${process.env.N8N_API_KEY}`, // Ensure you have set N8N_API_KEY in your environment variables
+        },
+      }
+    );
+    return response.data;
+  }
+
+  async deleteWorkflow(workflowId:string){
+    const response = await axios.delete(
+      `${process.env.N8N_SERVICE_URL}/api/v1/workflows/${workflowId}`,
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          'X-N8N-API-KEY': `${process.env.N8N_API_KEY}`, // Ensure you have set N8N_API_KEY in your environment variables
+        },
+      }
+    );
+    return response.data;
+  }
+
+
+
+
 }
