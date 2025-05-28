58a732ebe87397c2cad7c96bdecd7f892cc30fad
hissoune <khalidhissoune962@gmail.com>
2025-05-28 11:38:30 +0100

    feat(n8n): improve service logic

--- apps/n8n-service/src/n8n-service.service.ts
+++ apps/n8n-service/src/n8n-service.service.ts
@@ -78,6 +78,48 @@ export class N8nServiceService {
     return response.data;
   }
 
+  async activateWorkflow(workflowId: string) {
+    const response = await axios.post(
+      `${process.env.N8N_SERVICE_URL}/api/v1/workflows/${workflowId}/activate`,
+      {},
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          'X-N8N-API-KEY': `${process.env.N8N_API_KEY}`, // Ensure you have set N8N_API_KEY in your environment variables
+        },
+      }
+    );
+    return response.data;
+  }
+
+  async deactivateWorkflow(workflowId: string) {
+    const response = await axios.post(
+      `${process.env.N8N_SERVICE_URL}/api/v1/workflows/${workflowId}/deactivate`,
+      {},
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          'X-N8N-API-KEY': `${process.env.N8N_API_KEY}`, 
+        },
+      }
+    );
+    return response.data;
+  }
+
+  async handleWorkflowCreated(data: any) {
+    console.log('Workflow created:', data);
+
+    await axios.post(
+      `${process.env.N8N_SERVICE_URL}/webhook-test/webhook`,data,
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          'X-N8N-API-KEY': `${process.env.N8N_API_KEY}`,
+        }}).catch(error => {
+      console.error('Error activating workflow:', error);
+        })
+
+  }
 
 
 
