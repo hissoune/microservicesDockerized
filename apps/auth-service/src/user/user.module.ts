0ae0b7512d7a3daf755fc920b86a32eb088d9b4d
hissoune <khalidhissoune962@gmail.com>
2025-05-28 11:38:30 +0100

    refactor(auth/user): improve user module structure

--- apps/auth-service/src/user/user.module.ts
+++ apps/auth-service/src/user/user.module.ts
@@ -4,11 +4,32 @@ import { UserController } from './user.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { LogtailModule } from '../logtail/logtail.module';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([User]),
-    LogtailModule
+    LogtailModule,
+      ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: ['.env'],
+    }),
+       ClientsModule.register(
+      [
+        {
+            name: 'N8N_SERVICE',
+            transport: Transport.RMQ,
+            options: {
+              urls: [process.env.RABBITMQ_URL || 'amqp://localhost:5672/'],
+              queue: 'n8n_queue',
+              queueOptions: {
+                durable: true,
+              },
+            },
+          }
+    
+      ]),
   ],
   controllers: [UserController],
   providers: [UserService],
