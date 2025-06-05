8d48913b8ce15ab03144b674e980e631e6475de6
hissoune <khalidhissoune962@gmail.com>
2025-06-05 14:51:59 +0100

    refactor(auth/user): improve module structure

--- apps/auth-service/src/user/user.module.ts
+++ apps/auth-service/src/user/user.module.ts
@@ -8,9 +8,17 @@ import { ClientsModule, Transport } from '@nestjs/microservices';
 import { ConfigModule } from '@nestjs/config';
 import { PassportModule } from '@nestjs/passport';
 import { KeycloakStrategy } from './keycloak.strategy';
+import { PrometheusModule } from '@willsoto/nestjs-prometheus';
+import { Counter } from 'prom-client';
 
+export const apiRequestCounter = new Counter({
+  name: 'api_request_count',
+  help: 'Total number of API requests',
+  labelNames: ['method', 'path', 'status'],
+});
 @Module({
   imports: [
+PrometheusModule.register(),
     TypeOrmModule.forFeature([User]),
     PassportModule.register({ defaultStrategy: 'keycloak' }),
     LogtailModule,
@@ -35,6 +43,13 @@ import { KeycloakStrategy } from './keycloak.strategy';
       ]),
   ],
   controllers: [UserController],
-  providers: [UserService,KeycloakStrategy],
+  providers: [UserService,KeycloakStrategy,
+    {
+      provide: 'PROM_METRIC_API_REQUEST_COUNT',
+      useValue: apiRequestCounter,
+    },
+  ],
+    exports: ['PROM_METRIC_API_REQUEST_COUNT'],
+
 })
 export class UserModule {}
