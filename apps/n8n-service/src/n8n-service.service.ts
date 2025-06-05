bfe32093f06aae44ad3797064bbb90c7dada693c
hissoune <khalidhissoune962@gmail.com>
2025-06-05 14:52:00 +0100

    feat(n8n): enhance service logic

--- apps/n8n-service/src/n8n-service.service.ts
+++ apps/n8n-service/src/n8n-service.service.ts
@@ -7,6 +7,42 @@ export class N8nServiceService {
     return 'Hello World!';
   }
  
+  async getAnalitics() {
+    const response = await axios.get(
+      process.env.GRAFANA_SEARCH8URL || 'http://host:port/api/search',
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${process.env.GRAFANA_API_KEY}`, 
+        },
+      }
+    );
+
+     
+
+    const iframeslinks = await Promise.all(
+      response.data.map(async (dash: any) => {
+        const dashdata = await axios.get(
+          `${process.env.GRAFANA_DASHBOARD_URL}/${dash.uid}`,
+          {
+            headers: {
+              'Content-Type': 'application/json',
+              'Authorization': `Bearer ${process.env.GRAFANA_API_KEY}`,
+            },
+          }
+        );
+      console.log('dashdata', dashdata.data.dashboard.panels);
+      
+        return dashdata.data.dashboard.panels.map((panel: any) => {
+          return {
+            title: panel.title,
+            iframeLink: `${process.env.GRAFANA_URL}/d-solo/${dash.uid}?orgId=${dash.orgId}&panelId=${panel.id}&fullscreen&theme=dark`,
+          };
+        });
+      })
+    );
+    return iframeslinks.flat();
+  }
    
   async getWorkflows() {
     const response = await axios.get(
